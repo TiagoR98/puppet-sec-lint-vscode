c0a3de0642480d195885976df260ac7f158ed64e
Tiago Ribeiro <TiagoJose.Ribeiro@ctw.bmwgroup.com>
2021-05-02 03:30:13 +0100

    Communication made with lsp

--- src/extension.ts
+++ src/extension.ts
@@ -1,79 +1,75 @@
 'use strict';
 
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+	StreamInfo
+} from 'vscode-languageclient/node';
+
 import * as net from 'net';
 
-import {Trace} from 'vscode-jsonrpc';
-import { window, workspace, commands, ExtensionContext, Uri, TextDocumentChangeEvent, TextDocument, languages, Diagnostic, DiagnosticSeverity, Range, Position } from 'vscode';
-import { LanguageClient, LanguageClientOptions, StreamInfo, Position as LSPosition, Location as LSLocation } from 'vscode-languageclient/node';
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let client: LanguageClient;
 export function activate(context: ExtensionContext) {
-	const axios = require('axios');
-
-	let diagnosticCollection = languages.createDiagnosticCollection("stuff");
-	let diagnostics : Diagnostic[] = [];
 
 	// run puppet-sec-lint
 	const cp = require('child_process')
-	cp.exec('puppet-sec-lint', (err:any, stdout:any, stderr:any) => {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
-		if (err) {
-			console.log('error: ' + err);
+	//cp.exec('puppet-sec-lint', (err:any, stdout:any, stderr:any) => {
+	//	console.log('stdout: ' + stdout);
+	//	console.log('stderr: ' + stderr);
+	//	if (err) {
+	//		console.log('error: ' + err);
+	//	}
+	//});
+
+	const connectionInfo = {
+        port: 5007
+    };
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions = () => {
+        // Connect to language server via socket
+        const socket = net.connect(connectionInfo);
+        const result: StreamInfo = {
+            writer: socket,
+            reader: socket
+        };
+        return Promise.resolve(result);
+    };
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'puppet' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
-	});
+	};
 
-	let currOpenEditor = window.activeTextEditor;
-	if (currOpenEditor){
-	let currActiveDoc = currOpenEditor.document;
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
 
-	let onDidChangeDisposable = workspace.onDidSaveTextDocument((document: TextDocument)=>{
-		
-		if (document === currActiveDoc){
-				axios.post('http://localhost:9292', {
-					documentContent: document.getText()
-				})
-				.then((response:any) => {
-					console.log(response.data);
-
-					diagnostics = [];
-
-					response.data.forEach(function(sin:any) {
-						sin=JSON.parse(sin);
-
-						let range = new Range(sin.begin_line-1,sin.begin_char-1,sin.end_line-1,sin.end_char);
-					
-						let newDiagnostic = new Diagnostic(range, sin.message+" - [Recommendation] - "+sin.recommendation, DiagnosticSeverity.Warning);
-						newDiagnostic.source = "Puppet-sec-lint";
-
-						diagnostics.push(newDiagnostic);
-					});
-		
-					diagnosticCollection.set(document.uri, diagnostics);
-				});
-
-		}
-		else {
-			console.log('Non watched doc changed');
-		}
-	});
-	
-	let onDidCloseDisposable = workspace.onDidCloseTextDocument((closedDoc: TextDocument)=>{
-		if (closedDoc === currActiveDoc){
-			console.log('Watched doc was closed');
-		}
-		else {
-			console.log('non watched doc closed');
-		}
-	});
-
-	context.subscriptions.push(onDidChangeDisposable);
-	context.subscriptions.push(onDidCloseDisposable);
-	}
+	// Start the client. This will also launch the server
+	client.start();
 
 
 
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
