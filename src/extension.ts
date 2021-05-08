e9c2c6f33c69f94653e1d06346918c0ca9d2523c
Tiago Ribeiro <TiagoJose.Ribeiro@ctw.bmwgroup.com>
2021-05-08 01:13:12 +0100

    Automaticly selects a free port for language_server

--- src/extension.ts
+++ src/extension.ts
@@ -15,52 +15,60 @@ let cp: any;
 
 export async function activate(context: ExtensionContext) {
 
-	const connectionInfo = {
-        port: 5007
-    };
 
-	const serverOptions = () => {
-        // Connect to language server via socket
-        const socket = net.connect(connectionInfo);
-        const result: StreamInfo = {
-            writer: socket,
-            reader: socket
-        };
-        return Promise.resolve(result);
-    };
+	var fp = require("find-free-port");
+	fp(3000, function(err:any, freePort:any){
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'puppet' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
+		if(err)
+			{throw err;}
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'puppet-sec-lint',
-		'Puppet Security Linter',
-		serverOptions,
-		clientOptions
-	);
+		const connectionInfo = {
+			port: freePort
+		};
 
-	// run puppet-sec-lint
-	cp = require('child_process');
-	cp.exec('puppet-sec-lint', (err:any, stdout:any, stderr:any) => {
-		client.outputChannel.appendLine('stdout: ' + stdout);
-		client.outputChannel.appendLine('stderr: ' + stderr);
-		if (err) {
-			client.outputChannel.appendLine('Error running the ruby gem command:\n ' + err);
-			client.outputChannel.appendLine('Please make sure that the \'puppet-sec-lint\' gem is installed and working correctly.\n');
-		}
+		const serverOptions = () => {
+			// Connect to language server via socket
+			const socket = net.connect(connectionInfo);
+			const result: StreamInfo = {
+				writer: socket,
+				reader: socket
+			};
+			return Promise.resolve(result);
+		};
+
+		// Options to control the language client
+		const clientOptions: LanguageClientOptions = {
+			// Register the server for plain text documents
+			documentSelector: [{ scheme: 'file', language: 'puppet' }],
+			synchronize: {
+				// Notify the server about file changes to '.clientrc files contained in the workspace
+				fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+			}
+		};
+
+		// Create the language client and start the client.
+		client = new LanguageClient(
+			'puppet-sec-lint',
+			'Puppet Security Linter',
+			serverOptions,
+			clientOptions
+		);
+
+		// run puppet-sec-lint
+		cp = require('child_process');
+		cp.exec('puppet-sec-lint -p '+freePort, (err:any, stdout:any, stderr:any) => {
+			client.outputChannel.appendLine('stdout: ' + stdout);
+			client.outputChannel.appendLine('stderr: ' + stderr);
+			if (err) {
+				client.outputChannel.appendLine('Error running the ruby gem command:\n ' + err);
+				client.outputChannel.appendLine('Please make sure that the \'puppet-sec-lint\' gem is installed and working correctly.\n');
+			}
+		});
+
+		// Start the client. This will also launch the server
+		setTimeout(() => {  client.start(); }, 1000);
+
 	});
-
-	// Start the client. This will also launch the server
-	setTimeout(() => {  client.start(); }, 1000);
-
 
 
 }
